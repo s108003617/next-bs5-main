529dbfec24779888acad104b3f5c08e3e33d7085
ChenWei-Ming <168167809+ChenWei-Ming@users.noreply.github.com>
2024-07-30 19:07:21 +0800

    消除首頁麵包屑

--- components/layout/default-layout/index.js
+++ components/layout/default-layout/index.js
@@ -1,12 +1,16 @@
-// import MyNavbar from './my-navbar-nouse'
 import MyNavbarBS5 from './my-navbar'
 import MyFooter from './my-footer'
 import Head from 'next/head'
 import NextBreadCrumb from '@/components/common/next-breadcrumb'
 import { useLoader } from '@/hooks/use-loader'
+import { useRouter } from 'next/router'
 
 export default function DefaultLayout({ title = 'DigitalShop', children }) {
   const { loader } = useLoader()
+  const router = useRouter()
+
+  // 檢查當前路徑是否為首頁
+  const isHomePage = router.pathname === '/'
 
   return (
     <>
@@ -17,7 +21,9 @@ export default function DefaultLayout({ title = 'DigitalShop', children }) {
       <MyNavbarBS5 />
       <main className="flex-shrink-0 mt-3">
         <div className="container">
-          <NextBreadCrumb isHomeIcon isChevron bgClass="" />
+          {!isHomePage && ( // 只有在非首頁時顯示麵包屑
+            <NextBreadCrumb isHomeIcon isChevron bgClass="" />
+          )}
           {children}
         </div>
         {/* 全域的載入動畫指示器 */}
